add16ed3438ddabe66239af0df705b159cad69ac
seognil LC <seognil@gmail.com>
2022-04-15 14:31:15 +0800

    feat: update build config

--- examples/react-recoil-vite/vite.config.ts
+++ examples/react-recoil-vite/vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
+  build: { outDir: "../../build/react-recoil-vite" },
   plugins: [react()],
 });
