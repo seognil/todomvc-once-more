f5a937fcef7d407592dcf633493baaadede3e75c
seognil LC <seognil@gmail.com>
2023-03-23 13:47:27 +0800

    feat: astro add unocss

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import preact from "@astrojs/preact";
+import { defineConfig } from "astro/config";
+import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), preact()]
+  integrations: [mdx(), preact(), UnoCSS()],
 });
